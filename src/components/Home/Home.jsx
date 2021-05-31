c73afd506bd8f2a061b82f405f9ef904e2e4a7d7
Pavlo Bilousov <bilousov.p@dbbest.com>
2021-05-31 23:44:58 +0300

    Add basic fields for params

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,10 @@
 import {FormGroup, Input, Label} from "reactstrap";
+import React, { useState } from 'react';
+
+const INITIAL_FORM_MAIN_STATE = {
+    shapeOfArea: 'PARALLELP',
+    fieldOfUse: 'OTHER',
+};
 
 const shapeOptionsData = [
     {
@@ -21,6 +27,10 @@ const shapeOptionsData = [
 
 const fieldOfUseOptionsData = [
     {
+        label: 'Інше',
+        value: 'OTHER',
+    },
+    {
         label: 'Пересування',
         value: 'MOVE',
     },
@@ -28,34 +38,101 @@ const fieldOfUseOptionsData = [
         label: 'Зварювання',
         value: 'WELD',
     },
-    {
-        label: 'Інше',
-        value: 'OTHER',
-    },
 ];
 
 const getOptions = optionsData => optionsData.map(option =>
-    <option key={option.value} value={option.value}>{option.label}</option>)
+    <option key={option.value} value={option.value}>{option.label}</option>);
 
 const Home = () => {
+
+    const [formMainState, setFormMainState] = useState(INITIAL_FORM_MAIN_STATE);
+
+    const getAdditionalField = () => {
+        if (formMainState.fieldOfUse === 'MOVE') {
+            return (
+                <FormGroup className="form-group">
+                    <Label for="cargoCapacity">Корисне навантаження (кг):</Label>
+                    <Input type="number"
+                           name="cargoCapacity"
+                           id="cargoCapacity"
+                           onChange={({ target: { id, value }}) => updateFormState(id, value)} />
+                </FormGroup>
+            )
+        } else {
+            return (
+                <FormGroup className="form-group">
+                    <Label for="cargoCapacity">Вага робочго елементу (кг):</Label>
+                    <Input type="number"
+                           name="cargoCapacity"
+                           id="cargoCapacity"
+                           onChange={({ target: { id, value }}) => updateFormState(id, value)} />
+                </FormGroup>
+            )
+        }
+    };
+
+    const getFieldsForWeldParameters = () => (
+        <div className="form-div">
+            <h6>Параметри робочого елементу</h6>
+        </div>
+    )
+
+    const getFormWithAdditionalParams = () => {
+        switch (formMainState.fieldOfUse) {
+            case 'WELD':
+                return getFieldsForWeldParameters();
+            default:
+                return null;
+        }
+    }
+
+    const updateFormState = (fieldId, value) => {
+        const stateCopy = { ...formMainState };
+
+        setFormMainState({
+            ...stateCopy,
+            [fieldId]: value,
+        })
+    }
+
+    console.log('formState', formMainState);
+
     return (
         <div className="main-div">
-            <FormGroup className="form-group">
-                <Label for="shapeOfArea">Форма робочої зони:</Label>
-                <Input type="select"
-                       name="shapeOfAreaSelect"
-                       id="shapeOfArea"
-                       className="form-element"
-                       onChange={({ target: { value }}) => console.log(value)}>
-                    {getOptions(shapeOptionsData)}
-                </Input>
-            </FormGroup>
-            <FormGroup className="form-group">
-                <Label for="fieldOfUse">Сфера застосування:</Label>
-                <Input type="select" name="fieldOfUseSelect" id="fieldOfUse" className="form-element">
-                    {getOptions(fieldOfUseOptionsData)}
-                </Input>
-            </FormGroup>
+            <div className="form-div">
+                <h6>Основні параметри ПР</h6>
+                <FormGroup className="form-group">
+                    <Label for="shapeOfArea">Форма робочої зони:</Label>
+                    <Input type="select"
+                           name="shapeOfAreaSelect"
+                           id="shapeOfArea"
+                           className="form-element"
+                           defaultValue=''
+                           onChange={({ target: { id, value }}) => updateFormState(id, value)}>
+                        {getOptions(shapeOptionsData)}
+                    </Input>
+                </FormGroup>
+                <FormGroup className="form-group">
+                    <Label for="fieldOfUse">Сфера застосування:</Label>
+                    <Input type="select"
+                           name="fieldOfUseSelect"
+                           id="fieldOfUse"
+                           className="form-element"
+                           onChange={({ target: { id, value }}) => updateFormState(id, value)}>
+                        {getOptions(fieldOfUseOptionsData)}
+                    </Input>
+                </FormGroup>
+                <FormGroup className="form-group">
+                    <Label for="reachZone">Досяжність (см):</Label>
+                    <Input type="number"
+                           name="reachZone"
+                           id="reachZone"
+                           className="form-element"
+                           onChange={({ target: { id, value }}) => updateFormState(id, value)} />
+                </FormGroup>
+                {getAdditionalField()}
+            </div>
+            {getFormWithAdditionalParams()}
         </div>
     )
 }
